3384cefdc7033d17f7a1cc0763d02c0f16e8bc56
paulmalatesta <paulmalatesta@CHS-A104-DSK-02.CHS.chatham-nj.org>
2017-05-25 14:24:49 -0400

    some of minecell

--- sweepmine/src/edu/chatham/games/MineCell.java
+++ sweepmine/src/edu/chatham/games/MineCell.java
@@ -1,13 +1,22 @@
 package edu.chatham.games;
 
-public class MineCell 
-{
-	public MineCell()
+import acm.graphics.GImage;
+
+public class MineCell extends Cell{
+
+
+	public MineCell(int r, int c) 
 	{
-		super();
+		super(r, c);
+		GImage mineI = new GImage("mine");
+		mineI.scale(CELL_WIDTH/mineI.getWidth(),CELL_HEIGHT/mineI.getHeight());
+		add(mineI);
+		mineI.sendToBack();
 	}
+	
 	public void reveal()
 	{
-		
+		rect.setFilled(false);
+		//add boom every time it's a mime
 	}
 }
