292292bcfee4cf6d4eac3a2ad382c6e4fdc2d1af
jonathannamovic <jonathannamovic@CHS-A104-DSK-01.CHS.chatham-nj.org>
2017-05-30 13:01:55 -0400

    added the initial sweep out

--- sweepmine/src/edu/chatham/games/EmptyCell.java
+++ sweepmine/src/edu/chatham/games/EmptyCell.java
@@ -8,25 +8,31 @@ public class EmptyCell extends Cell {
 	public EmptyCell(int r, int c, Minesweeper d) 
 	{
 		super(r, c, d);
-		GImage mineI = new GImage("mine");
-		mineI.scale(CELL_WIDTH/mineI.getWidth(),CELL_HEIGHT/mineI.getHeight());
-		add(mineI);
-		mineI.sendToBack();
+		
 	}
 
 	@Override
 	public void reveal() {
-		isRevealed=true;
-		rect.setFilled(false);
-		for(int x=-1;x<2;x++){
-			for(int y=-1;y<2;y++){//the line after this one will cause an error when the square is in a corner but i dont know how to fix it
-				if(!(x+row<0||x+row>game.getBoard().getRows()||y+col<0||y+col>game.getBoard().getCols()))
-				if((game.getBoard().getCells()[row+x][col+y]) instanceof MineCell){
-					minesNear++;
+		if(!isRevealed){
+			isRevealed=true;
+			rect.setFilled(false);
+			for(int x=-1;x<2;x++){
+				for(int y=-1;y<2;y++){//the line after this one will cause an error when the square is in a corner but i dont know how to fix it
+					if(!(x+row<0||x+row>game.getBoard().getRows()||y+col<0||y+col>game.getBoard().getCols()))
+						if((game.getBoard().getCells()[row+x][col+y]) instanceof MineCell){
+							minesNear++;
+						}
 				}
 			}
+			if(minesNear==0){
+				for(int x=-1;x<2;x++){
+					for(int y=-1;y<2;y++){//the line after this one will cause an error when the square is in a corner but i dont know how to fix it
+						if(!(x+row<0||x+row>game.getBoard().getRows()||y+col<0||y+col>game.getBoard().getCols()))
+							game.getBoard().getCells()[row+x][col+y].reveal();
+					}
+				}	
+			}	
 		}
-		
 		//add the mines-near number to the image
 	}
 }
